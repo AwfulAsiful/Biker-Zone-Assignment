bdd8dd81ebaf6ffbe951768ce03c7ac3f73ee786
AwfulAsiful <awful872@gmail.com>
2024-10-26 22:04:54 +0600

    Navbar added and made responsive, added the dropdown menu feature as well

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      backgroundColor: {
+        headerBg: '#264653',
+        btnColor: '#E76F51',
+      },
+    },
+  },
+  plugins: [],
+ 
+
+
+}
